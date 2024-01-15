61ca15b68862801bc29ae5665dc46dba9603b928
sivajyothis7 <sivajyothis9446@gmail.com>
2024-01-15 09:57:08 +0530

    kkpp

--- develop/api.py
+++ develop/api.py
@@ -2,18 +2,18 @@ import frappe
 import json
 
 
-# @frappe.whitelist()
-# def add_item():
-#     data = json.loads(frappe.request.data)
+@frappe.whitelist()
+def add_item():
+    data = json.loads(frappe.request.data)
     
-#     item_doc = frappe.get_doc({
-#             "doctype": "Item",
-#             "item_code": data.get("item_code"),
-#             "item_name": data.get("item_name"),
-#             "item_group": data.get("item_group")
-#         })
+    item_doc = frappe.get_doc({
+            "doctype": "Item",
+            "item_code": data.get("item_code"),
+            "item_name": data.get("item_name"),
+            "item_group": data.get("item_group")
+        })
 
-#     item_doc.insert()
+    item_doc.insert()
 
 @frappe.whitelist()
 def add_customer():
