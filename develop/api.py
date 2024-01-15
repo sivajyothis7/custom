476c7d5453058276b4825455b282d78cb1cab3ba
sivajyothis7 <sivajyothis9446@gmail.com>
2024-01-15 09:51:21 +0530

    kkpp

--- develop/api.py
+++ develop/api.py
@@ -1,32 +1,19 @@
 import frappe
 import json
 
+
 # @frappe.whitelist()
-# def add_item(code,name,group):
-#     add_item = frappe.get_doc({"doctype":"Item",
-#         "item_code": code,
-#         "item_name": name,
-#         "item_group": group,
-
-
-#     })
-
-#     add_item.insert()
-#     frappe.db.commit()
-#     return add_item
-
-@frappe.whitelist()
-def add_item():
-    data = json.loads(frappe.request.data)
+# def add_item():
+#     data = json.loads(frappe.request.data)
     
-    item_doc = frappe.get_doc({
-            "doctype": "Item",
-            "item_code": data.get("item_code"),
-            "item_name": data.get("item_name"),
-            "item_group": data.get("item_group")
-        })
+#     item_doc = frappe.get_doc({
+#             "doctype": "Item",
+#             "item_code": data.get("item_code"),
+#             "item_name": data.get("item_name"),
+#             "item_group": data.get("item_group")
+#         })
 
-    item_doc.insert()
+#     item_doc.insert()
 
 @frappe.whitelist()
 def add_customer():
@@ -39,7 +26,4 @@ def add_customer():
 
     new_cus.insert()
 
-    # return customer_doc
-    # print(f"\n\n{data}\n\n")
-
-    # return
+  
