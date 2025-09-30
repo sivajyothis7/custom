1c404365ef9e1af5222c36c09555c209854a99b3
sivajyothis7 <sivajyothis9446@gmail.com>
2025-09-30 21:12:35 +0530

    mode

--- develop/develop/report/item_wise_sales_register_new/item_wise_sales_register_new.py
+++ develop/develop/report/item_wise_sales_register_new/item_wise_sales_register_new.py
@@ -73,7 +73,7 @@ def _execute(filters=None, additional_table_columns=None, additional_conditions=
             "pos_invoice": d.pos_invoice,  
             **get_values_for_columns(additional_table_columns, d),
             "debit_to": d.debit_to,
-			"mode_of_payment": ", ".join(mode_of_payments.get(d.pos_invoice, [])) if d.pos_invoice else ""
+			"mode_of_payment": ", ".join(mode_of_payments.get(d.pos_invoice, [])) if d.pos_invoice else "",
             "territory": d.territory,
             "project": d.project,
             "company": d.company,
