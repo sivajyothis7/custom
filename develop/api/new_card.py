63583b384333c893c8d59d2758e2370d65e34809
Abid Khan <abidkh.ak64@gmail>
2025-10-11 22:26:11 +0530

    BackTick Issue fixed in Queries

--- develop/api/new_card.py
+++ develop/api/new_card.py
@@ -1,296 +1,97 @@
 import frappe
 from frappe.utils import today
 
+# -------------------------- PAYMENT MODE WISE --------------------------
+
 @frappe.whitelist()
 def get_total_mada_payments():
-    posting_date = today()
-    rows = frappe.db.sql("""
-        SELECT
-            SUM(sip.amount) AS total_amount
-        FROM
-            tabSales Invoice Payment sip
-        JOIN
-            tabPOS Invoice pi ON sip.parent = pi.name
-        WHERE
-            sip.parenttype = 'POS Invoice'
-            AND sip.mode_of_payment = 'Mada'
-            AND pi.posting_date = %s
-            AND pi.docstatus = 1
-    """, (posting_date,), as_dict=True)
-
-    total = rows[0]['total_amount'] if rows else 0
-
-    return {
-        "value": total,
-        "fieldtype": "Currency"
-    }
+    return get_total_by_mode_of_payment("Mada")
 
 @frappe.whitelist()
 def get_total_master_payments():
-    posting_date = today()
-    rows = frappe.db.sql("""
-        SELECT
-            SUM(sip.amount) AS total_amount
-        FROM
-            tabSales Invoice Payment sip
-        JOIN
-            tabPOS Invoice pi ON sip.parent = pi.name
-        WHERE
-            sip.parenttype = 'POS Invoice'
-            AND sip.mode_of_payment = 'Master'
-            AND pi.posting_date = %s
-            AND pi.docstatus = 1
-    """, (posting_date,), as_dict=True)
-
-    total = rows[0]['total_amount'] if rows else 0
-
-    return {
-        "value": total,
-        "fieldtype": "Currency"
-    }
+    return get_total_by_mode_of_payment("Master")
 
 @frappe.whitelist()
 def get_total_card_payments():
-    posting_date = today()
-    rows = frappe.db.sql("""
-        SELECT
-            SUM(sip.amount) AS total_amount
-        FROM
-            tabSales Invoice Payment sip
-        JOIN
-            tabPOS Invoice pi ON sip.parent = pi.name
-        WHERE
-            sip.parenttype = 'POS Invoice'
-            AND sip.mode_of_payment = 'Card'
-            AND pi.posting_date = %s
-            AND pi.docstatus = 1
-    """, (posting_date,), as_dict=True)
-
-    total = rows[0]['total_amount'] if rows else 0
-
-    return {
-        "value": total,
-        "fieldtype": "Currency"
-    }
+    return get_total_by_mode_of_payment("Card")
 
 @frappe.whitelist()
 def get_total_cash_payments():
-    posting_date = today()
-    rows = frappe.db.sql("""
-        SELECT
-            SUM(sip.amount) AS total_amount
-        FROM
-            tabSales Invoice Payment sip
-        JOIN
-            tabPOS Invoice pi ON sip.parent = pi.name
-        WHERE
-            sip.parenttype = 'POS Invoice'
-            AND sip.mode_of_payment = 'Cash'
-            AND pi.posting_date = %s
-            AND pi.docstatus = 1
-    """, (posting_date,), as_dict=True)
-
-    total = rows[0]['total_amount'] if rows else 0
-
-    return {
-        "value": total,
-        "fieldtype": "Currency"
-    }
+    return get_total_by_mode_of_payment("Cash")
 
 @frappe.whitelist()
 def get_total_american_payments():
-    posting_date = today()
-    rows = frappe.db.sql("""
-        SELECT
-            SUM(sip.amount) AS total_amount
-        FROM
-            tabSales Invoice Payment sip
-        JOIN
-            tabPOS Invoice pi ON sip.parent = pi.name
-        WHERE
-            sip.parenttype = 'POS Invoice'
-            AND sip.mode_of_payment = 'American Express'
-            AND pi.posting_date = %s
-            AND pi.docstatus = 1
-    """, (posting_date,), as_dict=True)
-
-    total = rows[0]['total_amount'] if rows else 0
-
-    return {
-        "value": total,
-        "fieldtype": "Currency"
-    }
+    return get_total_by_mode_of_payment("American Express")
 
 @frappe.whitelist()
 def get_total_visa_payments():
-    posting_date = today()
-    rows = frappe.db.sql("""
-        SELECT
-            SUM(sip.amount) AS total_amount
-        FROM
-            tabSales Invoice Payment sip
-        JOIN
-            tabPOS Invoice pi ON sip.parent = pi.name
-        WHERE
-            sip.parenttype = 'POS Invoice'
-            AND sip.mode_of_payment = 'Visa'
-            AND pi.posting_date = %s
-            AND pi.docstatus = 1
-    """, (posting_date,), as_dict=True)
-
-    total = rows[0]['total_amount'] if rows else 0
-
-    return {
-        "value": total,
-        "fieldtype": "Currency"
-    }
+    return get_total_by_mode_of_payment("Visa")
 
 @frappe.whitelist()
 def get_total_bank_payments():
-    posting_date = today()
+    return get_total_by_mode_of_payment("Bank Transfer")
 
-    rows = frappe.db.sql("""
-        SELECT
-            SUM(sip.amount) AS total_amount
-        FROM
-            tabSales Invoice Payment sip
-        JOIN
-            tabPOS Invoice pi ON sip.parent = pi.name
-        WHERE
-            sip.parenttype = 'POS Invoice'
-            AND sip.mode_of_payment = 'Bank Transfer'
-            AND pi.posting_date = %s
-            AND pi.docstatus = 1
-    """, (posting_date,), as_dict=True)
+# -------------------------- STORE WISE --------------------------
 
-    total = rows[0]['total_amount'] if rows else 0
-
-    return {
-        "value": total,
-        "fieldtype": "Currency"
-    }
-
-########################## Store Wise ########################
 @frappe.whitelist()
-
 def get_total_fleurs_payments():
-    posting_date = today()
-
-    rows = frappe.db.sql("""
-        SELECT SUM(sip.amount) AS total_amount
-        FROM tabSales Invoice Payment sip 
-        JOIN tabPOS Invoice pi ON sip.parent = pi.name
-        WHERE 
-        sip.parenttype = 'POS Invoice'
-        AND pi.pos_profile = 'FLEURS DE VIE'
-        AND pi.posting_date = %s
-        AND pi.docstatus = 1
-        """, (posting_date,), as_dict = True)
-    total = rows[0]['total_amount'] if rows else 0
-
-    return {
-        "value" : total,
-        "fieldtype" : "Currency"
-    }
-               
+    return get_total_by_pos_profile("FLEURS DE VIE")
 
 @frappe.whitelist()
 def get_total_camilia_payments():
-    posting_date = today()
-
-    rows = frappe.db.sql("""
-        SELECT
-            SUM(sip.amount) AS total_amount
-        FROM
-            tabSales Invoice Payment sip
-        JOIN
-            tabPOS Invoice pi ON sip.parent = pi.name
-        WHERE
-            sip.parenttype = 'POS Invoice'
-            AND pi.pos_profile = 'Camilia Store'
-            AND pi.posting_date = %s
-            AND pi.docstatus = 1
-    """, (posting_date,), as_dict=True)
-
-    total = rows[0]['total_amount'] if rows else 0
-
-    return {
-        "value": total,
-        "fieldtype": "Currency"
-    }
+    return get_total_by_pos_profile("Camilia Store")
 
 @frappe.whitelist()
 def get_total_concept_payments():
-    posting_date = today()
-
-    rows = frappe.db.sql("""
-        SELECT
-            SUM(sip.amount) AS total_amount
-        FROM
-            tabSales Invoice Payment sip
-        JOIN
-            tabPOS Invoice pi ON sip.parent = pi.name
-        WHERE
-            sip.parenttype = 'POS Invoice'
-            AND pi.pos_profile = 'Concept Store'
-            AND pi.posting_date = %s
-            AND pi.docstatus = 1
-    """, (posting_date,), as_dict=True)
-
-    total = rows[0]['total_amount'] if rows else 0
-
-    return {
-        "value": total,
-        "fieldtype": "Currency"
-    }
+    return get_total_by_pos_profile("Concept Store")
 
 @frappe.whitelist()
 def get_total_chelsie_payments():
-    posting_date = today()
-
-    rows = frappe.db.sql("""
-        SELECT
-            SUM(sip.amount) AS total_amount
-        FROM
-            tabSales Invoice Payment sip
-        JOIN
-            tabPOS Invoice pi ON sip.parent = pi.name
-        WHERE
-            sip.parenttype = 'POS Invoice'
-            AND pi.pos_profile = 'Chelsie Lane'
-            AND pi.posting_date = %s
-            AND pi.docstatus = 1
-    """, (posting_date,), as_dict=True)
-
-    total = rows[0]['total_amount'] if rows else 0
-
-    return {
-        "value": total,
-        "fieldtype": "Currency"
-    }
+    return get_total_by_pos_profile("Chelsie Lane")
 
 @frappe.whitelist()
 def get_total_boulevard_payments():
-    posting_date = today()
+    return get_total_by_pos_profile("Boulevard Runway")
 
+# -------------------------- HELPER FUNCTIONS --------------------------
+
+def get_total_by_mode_of_payment(mode_of_payment):
+    """Return total payments by Mode of Payment for today's POS Invoices"""
+    posting_date = today()
     rows = frappe.db.sql("""
         SELECT
             SUM(sip.amount) AS total_amount
         FROM
-            tabSales Invoice Payment sip
+            `tabSales Invoice Payment` AS sip
         JOIN
-            tabPOS Invoice pi ON sip.parent = pi.name
+            `tabPOS Invoice` AS pi ON sip.parent = pi.name
         WHERE
             sip.parenttype = 'POS Invoice'
-            AND pi.pos_profile = 'Boulevard Runway'
+            AND sip.mode_of_payment = %s
             AND pi.posting_date = %s
             AND pi.docstatus = 1
-    """, (posting_date,), as_dict=True)
+    """, (mode_of_payment, posting_date), as_dict=True)
 
-    total = rows[0]['total_amount'] if rows else 0
+    total = rows[0]['total_amount'] if rows and rows[0]['total_amount'] else 0
+    return {"value": total, "fieldtype": "Currency"}
 
-    return {
-        "value": total,
-        "fieldtype": "Currency"
-    }
 
+def get_total_by_pos_profile(pos_profile):
+    """Return total payments by POS Profile (Store) for today's POS Invoices"""
+    posting_date = today()
+    rows = frappe.db.sql("""
+        SELECT
+            SUM(sip.amount) AS total_amount
+        FROM
+            `tabSales Invoice Payment` AS sip
+        JOIN
+            `tabPOS Invoice` AS pi ON sip.parent = pi.name
+        WHERE
+            sip.parenttype = 'POS Invoice'
+            AND pi.pos_profile = %s
+            AND pi.posting_date = %s
+            AND pi.docstatus = 1
+    """, (pos_profile, posting_date), as_dict=True)
+
+    total = rows[0]['total_amount'] if rows and rows[0]['total_amount'] else 0
+    return {"value": total, "fieldtype": "Currency"}
